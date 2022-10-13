34a038008b2fdfcdaadb53542755f05b9bf0f5d5
Brian Massey <bmassey5@stu.jsu.edu>
2022-10-13 14:47:52 -0500

    Cole was here

---
+++ src/edu/jsu/mcis/cs310/tas_fa22/Shift.java
@@ -0,0 +1,21 @@
+package edu.jsu.mcis.cs310.tas_fa22;
+
+public class Shift {
+
+    private final int lunchduration;
+    private final int shiftduration;
+
+    public Shift(int lunchduration, int shiftduration){
+        this.lunchduration = lunchduration;
+        this.shiftduration = shiftduration;
+
+    }
+    
+
+}
+//hi
+
+//hello howdy dooo
+// hola
+
+
