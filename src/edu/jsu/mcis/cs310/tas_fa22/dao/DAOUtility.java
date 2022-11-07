7e957043461dae5cf12553aaa9abb7635a274e56
Jailon Lawrence <jlawrence2@stu.jsu.edu>
2022-11-07 12:15:22 -0600

    Started other utility method still in progress

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOUtility.java
@@ -4,9 +4,29 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import edu.jsu.mcis.cs310.tas_fa22.Punch;
+import edu.jsu.mcis.cs310.tas_fa22.PunchAdjustmentType;
+import edu.jsu.mcis.cs310.tas_fa22.Shift;
 import org.json.simple.*;
 
 public final class DAOUtility {
+
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
+        int minutes = 0;
+
+        for (int i = 0; i < dailypunchlist.size(); i += 2) {
+            Punch punch1 = dailypunchlist.get(i);
+            Punch punch2 = dailypunchlist.get(i+1);
+
+            LocalTime time1 = punch1.getAdjustedtimestamp().toLocalTime();
+            LocalTime time2 = punch2.getAdjustedtimestamp().toLocalTime();
+
+            minutes += (int) time1.until(time2, ChronoUnit.MINUTES); // Makes Shift1Weekday, Shift1Weekend
+        }
+
+        minutes -= shift.getlunchduration(); // Makes Shift2Weekday work
+
+        return minutes;
+    }
 
 
     public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
