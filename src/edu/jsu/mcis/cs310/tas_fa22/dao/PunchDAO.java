bf2e2443728f547002015b49cb194cc92ae0a6a5
Chloe Renfroe <crenfroe1@stu.jsu.edu>
2022-11-07 00:17:41 -0600

    Added list method to PunchDAO for range of dates and added corresponding test.

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
@@ -18,7 +18,7 @@ import java.util.Comparator;
 public class PunchDAO {
 
     private static final String QUERY_FIND = "SELECT * FROM event WHERE id = ?";
-    private static final String QUERY_CREATE = "INSERT INTO event (terminalid, badgeid, timestamp, eventtypeid) VALUES (?, ?, ?, ?)";
+    private static final String QUERY_CREATE = "INSERT INTO event VALUES (?, ?, ?, ?, ?)";
     private static final String QUERY_LIST = "SELECT * FROM event WHERE badgeid = ?";
 
     private final DAOFactory daoFactory;
@@ -86,51 +86,44 @@ public class PunchDAO {
     public int create(Punch punch) {
 
         PreparedStatement ps = null;
-        ResultSet keys;
-        int id = 0;
         int update = 0;
 
-        EmployeeDAO employeeDAO = new EmployeeDAO(daoFactory);
+        try {
 
-        int punchTerminalid = punch.getTerminalid();
-        int departmentTerminalid = employeeDAO.find(punch.getBadge()).getDepartment().getTerminal_id();
+            EmployeeDAO edao = new EmployeeDAO(daoFactory);
 
-        if (punchTerminalid == departmentTerminalid) {
-            try {
+            int pTermid = punch.getTerminalid();
+            int dTermid = edao.find(punch.getBadge()).getDepartment().getTerminal_id();
 
+            if (pTermid == dTermid) {
 
                 Connection conn = daoFactory.getConnection();
 
+
                 if (conn.isValid(0)) {
-                    ps = conn.prepareStatement(QUERY_CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
-                    ps.setInt(1, punch.getTerminalid()); // terminalid
-                    ps.setString(2, punch.getBadge().getId()); // badgeid
-                    ps.setString(3, punch.getOriginaltimestamp().toString()); // timestamp
-                    ps.setInt(4, punch.getPunchtype().ordinal()); // eventtype
+                    ps = conn.prepareStatement(QUERY_CREATE);
+                    ps.setInt(1, punch.getId()); // id
+                    ps.setInt(2, punch.getTerminalid()); // terminalid
+                    ps.setString(3, punch.getBadge().getId()); // badgeid
+                    ps.setString(4, punch.getOriginaltimestamp().toString()); // timestamp
+                    ps.setInt(5, punch.getPunchtype().ordinal()); // eventtype
 
                     update = ps.executeUpdate();
-
-                    if (update == 1) {
-                        keys = ps.getGeneratedKeys();
-                        if (keys.next()) {
-                            id = keys.getInt(1);
-                        }
-                    }
                 }
-            } catch (SQLException e) {
-                throw new DAOException(e.getMessage());
-            } finally {
-                if (ps != null) {
-                    try {
-                        ps.close();
-                    } catch (SQLException e) {
-                        throw new DAOException(e.getMessage());
-                    }
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
                 }
             }
         }
 
-        return id;
+        return update;
     }
 
     public ArrayList<Punch> list(Badge badge, LocalDate localDate) {
